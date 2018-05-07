cd6292b1628a3469d0e840088565cdcfc389aa1c
Matt Maher <mattmaher0@gmail.com>
2018-05-07 14:59:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,23 +16,29 @@ WIN_COMBINATIONS = [
   [6, 4, 2],
   ]
 
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-     false
-  elsif board[index] == "X" || board[index] == "O"
-     true 
-  end   
-end  
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index-1] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
 
 def valid_move?(board, index)
-  if position_taken?(board, index) 
-    false
-  elsif index.between?(0, 8) == false
-    false
-  else 
-    true
-  end  
-end  
+  index.between?(0,8) && !position_taken?(board, index)
+end 
 
 def empty_board(board) 
   board.all? {|idx| idx == " "} 

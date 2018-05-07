5ea7e0a6a7a48bdb7ab99d53a67a5cfac9c8fcf3
Matt Maher <mattmaher0@gmail.com>
2018-05-07 14:15:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -136,10 +136,10 @@ def play(board)
   until turn_count == 9
     turn(board)
     turn_count += 1
-    if won?(board)
+  end 
+  if won?(board)
       puts "Congratulations! #{winner}"
     else draw?(board)
       puts "It was a draw!"
     end 
-  end  
 end  

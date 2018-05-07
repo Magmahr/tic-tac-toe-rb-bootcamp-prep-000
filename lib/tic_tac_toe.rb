c4adef1d1cbefa16ecaf26ae7334719175acc195
Matt Maher <mattmaher0@gmail.com>
2018-05-07 14:11:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,7 +19,8 @@ def empty_board(board)
 end  
 
 def won?(board)
-  WIN_COMBINATIONS.each  do |win_combo|
+<<<<<<< HEAD
+  WIN_COMBINATIONS.each {|win_combo|
     index_0 = win_combo[0]
     index_1 = win_combo[1]
     index_2 = win_combo[2]
@@ -28,25 +29,26 @@ def won?(board)
     position_2 = board[index_1]
     position_3 = board[index_2]
 
-      if position_1 == "X" && position_2 == "X" && position_3   == "X"
-        return win_combo
-      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combo
-      end
-  end
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
   return false
-
+end 
+=======
   if empty_board(board) 
     return false
   end  
   if draw?(board) 
     return false
   end  
- WIN_COMBINATIONS.each do |array|
+WIN_COMBINATIONS.each do |array|
     if board[array[0]] == board[array[1]] && board[array[1]] == board[array[2]] && position_taken?(board, array[0])
       return array 
      end  
-  end
+  end    
 end
   
 

953ae9c8069f337b94cf674bcfe38c9056cf7ac9
Matt Maher <mattmaher0@gmail.com>
2018-05-07 14:42:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,12 +23,16 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def move(board, location, current_player = "X")
-  board[location.to_i-1] = current_player
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
 
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, index)
+  board[index] != " " && board[index] != ""
 end
 
 def valid_move?(board, position)

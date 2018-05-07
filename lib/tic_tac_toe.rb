49fac5c2dc8b8391aa76d35f3fd5c26349291919
Matt Maher <mattmaher0@gmail.com>
2018-05-07 14:58:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,6 +16,24 @@ WIN_COMBINATIONS = [
   [6, 4, 2],
   ]
 
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+     false
+  elsif board[index] == "X" || board[index] == "O"
+     true 
+  end   
+end  
+
+def valid_move?(board, index)
+  if position_taken?(board, index) 
+    false
+  elsif index.between?(0, 8) == false
+    false
+  else 
+    true
+  end  
+end  
+
 def empty_board(board) 
   board.all? {|idx| idx == " "} 
 end  
